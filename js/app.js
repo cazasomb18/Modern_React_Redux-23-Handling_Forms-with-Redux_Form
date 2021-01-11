ff514ace24515a4dd054a2b80ede78c669b573fa
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2021-01-10 21:44:00 -0600

    moving notes to section 24, section 23 complete,form errors functions as expected

--- js/app.js
+++ js/app.js
@@ -493,39 +493,3 @@
 
 	//Now we need to work on onSubmit(){} to make sure it's acutally pinging our api:
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//Creating Streams
-
-	//What are we doing once user submits the form?
-
-	//We're going to focus on the overall architecure of our project:
-
-	//Remember this diagram (appArchitecture.png)?
-										//APIServer that knows
-										//which streams are
-										//broadcasting		--> [Viewer's Browser]
-//											^^^					[ReactApp that can] 
-//											^^^					[create and browse] 
-//											^^^						[streams]	
-	//Streamer's Computer: running OBS --> [RTMP Server] 	<-- [Viewer's Browser]
-//		creating stream: id 2			show me streamid: 2
-
-
-	//API Server: has a plain list of records, each record will represent one stream:
-		// {id: 1, title: 'My Stream', description: 'some stream'}
-		// {id: 2, title: 'Code Stream', description: 'Coding'}
-
-	//Streamer running OBS: I am creating stream id 2
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//REST-ful Conventions
-
-	//We're going to have to start working on the API Server
